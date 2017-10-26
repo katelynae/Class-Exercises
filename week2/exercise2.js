5af25bdeca80e260ed957f2e5b200196f5d3bfbf
Katelyn <katelyn.aeklund@gmail.com>
2017-10-26 15:51:05 +0100

    week 2 exercises

---
+++ week2/exercise2.js
@@ -0,0 +1,26 @@
+var people = 20;
+var cats = 30;
+var dogs = 15;
+
+if(people < cats)
+    console.log("Too many cats! The world is doomed!")
+
+if(people > cats)
+    console.log("Not many cats! The world is saved!")
+
+if(people < dogs)
+    console.log("The world is drooled on!")
+
+if(people > dogs)
+    console.log("The world is dry!")
+
+dogs += 5
+
+if(people >= dogs)
+    console.log("People are greater than equal to dogs.")
+
+if(people <= dogs)
+    console.log("People are less than equal to dogs.")
+
+if(people == dogs)
+    console.log("People are dogs.")
